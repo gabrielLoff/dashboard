885bbe6636d5cac2b86c410b2e423ed15b41d682
gloff <gabiloff7@gmail.com>
2026-07-28 21:08:46 +0200

    chore(#61): delete dead adapter layer
    Closes #61

--- packages/server/src/adapters/index.ts
+++
@@ -1,8 +0,0 @@
-export { mockWeatherFetcher } from './weather.ts';
-export type { WeatherFetcher } from './weather.ts';
-export { mockNewsFetcher } from './news.ts';
-export type { NewsFetcher } from './news.ts';
-export { mockAgendaFetcher } from './agenda.ts';
-export type { AgendaFetcher } from './agenda.ts';
-export { mockGamesFetcher } from './games.ts';
-export type { GamesFetcher } from './games.ts';
